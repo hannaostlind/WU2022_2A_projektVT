fdc511a57b08f8d6d777a921d91fe19218fd409b
hannaostlind <hanna.ostlind@elev.ga.ntig.se>
2022-03-08 18:51:19 +0100

    Funkar det nu?

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const nav = document.getElementById('nav-links')
-const burger = document.getElementById('burger')
-
-function toggleMenu() {
-    nav.classList.toggle('nav-active')
-    burger.classList.toggle('crossed_line')
-}
-
-burger.addEventListener('click', toggleMenu)
